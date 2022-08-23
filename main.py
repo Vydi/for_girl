2928265f89145509dcc7838065766618eeeaadf9
name <name@gmail.com>
2022-08-23 10:45:58 +0300

    sub update

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from datetime import datetime
 
 from config import TOKEN_BOT, admin
 from aiogram import Bot, Dispatcher, executor, types
-from my_parser import parser_fun
+from my_parser import post_for_tg, add_subb, all_sub_f
 import random
 
 bot = Bot(token=TOKEN_BOT)
 dp = Dispatcher(bot)
 
-posts_arr = parser_fun()
-new_user = []
+posts_arr = post_for_tg()
 
 
 @dp.message_handler(commands='start')
@@ -18,15 +17,17 @@ async def start(message: types.Message):
     await message.answer('Любимка моя, чтоб не скучала \u2764\uFE0F')
     if str(message.from_user.id) != admin:
         print('not mine')
-        new_user.append(message.from_user.id)
+        add_subb(message.from_user.id)
     else:
         print('admin!')
 
 
-async def posts(wait_for=1800):
+async def posts(wait_for=10):
     while True:
+        subscribers = all_sub_f()
+        print(subscribers)
         await asyncio.sleep(wait_for)
-        for i in new_user:
+        for i in subscribers:
             await bot.send_message(i, text=random.choice(posts_arr))
             print('------------------------------------------', datetime.now(), i)
 
--- my_parser.py
+++ my_parser.py
@@ -1,3 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('post.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS tab_1 (id integer Primary key autoincrement, post text)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS tab_2 (id integer Primary key autoincrement, sub text)''')
+
+
 def parser_fun():
     import requests
     from bs4 import BeautifulSoup
@@ -15,4 +23,38 @@ def parser_fun():
     return all_compliment
 
 
+def db_post():
+    for i in parser_fun():
+        cursor.execute('''INSERT INTO tab_1 (post) VALUES(?)''', (i,))
+    conn.commit()
+    print('Success post')
 
+
+def post_for_tg():
+    cursor.execute('''SELECT post FROM tab_1''')
+    k = cursor.fetchall()
+    all_posts = []
+    for i in k:
+        all_posts.append(i[0])
+    return all_posts
+
+
+def add_subb(user_id):
+    cursor.execute('''SELECT sub FROM tab_2 WHERE sub = ?''', (user_id,))
+    k = cursor.fetchall()
+    if len(k) > 0:
+        print('Уже есть в бд')
+    else:
+        cursor.execute('''INSERT INTO tab_2 (sub) VALUES(?)''', (user_id,))
+        conn.commit()
+
+
+def all_sub_f():
+    all_sub = []
+    cursor.execute('''SELECT sub FROM tab_2''')
+    k = cursor.fetchall()
+    for i in k:
+        all_sub.append(i[0])
+    return all_sub
+# post_for_tg()
+# db_post()
